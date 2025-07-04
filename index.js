340cd7b517d7dabd9197e92260ec6448b2602af7
nadu1177 <snadimhossain1@gmail.com>
2025-07-04 09:12:57 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Poppins, sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>📒 Welcome to Note-by-Nadu</h1>
+      <p>Personal & Colorful Note App Powered by Firebase + AI</p>
+    </div>
+  );
+}
